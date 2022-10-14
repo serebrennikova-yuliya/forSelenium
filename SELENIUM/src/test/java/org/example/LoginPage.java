9d82c87bbf6556513dafa21d003901fdb3bf78fa
serebrennikova-yuliya <kis5.5@mail.ru>
2022-10-14 13:44:50 +0700

    001

---
+++ SELENIUM/src/test/java/org/example/LoginPage.java
@@ -0,0 +1,43 @@
+package org.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class LoginPage {
+    /**
+     * конструктор класса, занимающийся инициализацией полей класса
+     */
+    public WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver; }
+    /**
+     * определение локатора поля ввода логина
+     */
+    @FindBy(xpath="//input[@name='username']")
+    private WebElement loginField;
+    /**
+     * определение локатора кнопки входа в аккаунт
+     */
+    @FindBy(xpath="//span[contains(.,'Войти')]")
+    private WebElement loginBtn;
+    /**
+     * определение локатора поля ввода пароля
+     */
+    @FindBy(xpath="//input[@name='password']")
+    private WebElement passwdField;
+    /**
+     * метод для ввода логина
+     */
+    public void inputLogin(String login) {
+        loginField.sendKeys(login); }
+    /**
+     * метод для ввода пароля
+     */
+    public void inputPasswd(String passwd) {
+        passwdField.sendKeys(passwd); }
+    /**
+     * метод для осуществления нажатия кнопки входа в аккаунт
+     */
+    public void clickLoginBtn() {
+        loginBtn.click(); } }
