e4ac30bb2078d91c1194eaf11c9cf48b1a0050ff
serebrennikova-yuliya <kis5.5@mail.ru>
2022-10-19 13:00:21 +0700

    Задание 6. Сделайте сценарий, проходящий по всем разделам админки

--- SELENIUM/src/test/java/org/example/LoginTest.java
+++ SELENIUM/src/test/java/org/example/LoginTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+import org.openqa.selenium.By;
 public class LoginTest {
     public static LoginPage loginPage;
     public static ProfilePage profilePage;
@@ -47,8 +50,22 @@ public class LoginTest {
         //получаем отображаемый логин
 //        String user = profilePage.getUserName();
         //и сравниваем его с логином из файла настроек
- //       Assert.assertEquals(ConfProperties.getProperty("login"), user);
+        //       Assert.assertEquals(ConfProperties.getProperty("login"), user);
+        List<WebElement> menuItems = driver.findElements(By.cssSelector("li#app- span.name"));
+        for (int i = 0; i < menuItems.size(); i++) {
+            driver.findElement(By.cssSelector("li#app-:nth-child(" + (i + 1) + ")")).click();
+
+            if (driver.findElements(By.cssSelector("ul.docs")).size() == 0) {
+                Assert.assertTrue("The title is not shown on the page.", driver.findElement(By.cssSelector("h1")).isDisplayed());
+            } else {
+                List<WebElement> subMenuItems = driver.findElements(By.cssSelector("ul.docs li"));
+                for (int j = 0; j < subMenuItems.size(); j++) {
+                    driver.findElement(By.cssSelector("ul.docs li:nth-child(" + (j + 1) + ")")).click();
+                    Assert.assertTrue("The title is not shown on the page.", driver.findElement(By.cssSelector("h1")).isDisplayed());
+                }
+            }
         }
+    }
     /**
      * осуществление выхода из аккаунта с последующим закрытием окна браузера
      */
